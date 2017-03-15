d679679a9182c6e10e96425f681d1f970875016f
carlosrodriguez96 <esemismo96@gmail.com>
2017-03-15 09:53:45 -0500

    Update llamado-php.php

--- www/llamado-php.php
+++ www/llamado-php.php
@@ -1,5 +1,9 @@
 <?php
-
+    /**
+    * Desarrollado por:
+    * Carlos Arturo rodriguez
+    * Jhon Jairo Salazar 
+    */
     /**
      * Este php me permite usar el poder del php con un ejemplo de la tecnología AngularJS...
      * incluso desde el administrador de este sitio.
@@ -7,10 +11,10 @@
     include'BD.php';
     $nuevo_obj=new BD();    // llama la clase BD
            
-     if( isset( $_GET[ 'cadena' ] ) )
+     if( isset( $_GET[ 'cadena' ] ) )//se pregunta si llegan valores 
     {     
         $valores=$_GET['cadena'];
-        echo  $nuevo_obj->consultar($valores);
+        echo  $nuevo_obj->consultar($valores);//funcion que consulta la base de datos
         //echo $sql;
     }
   ?>
