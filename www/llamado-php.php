e5265ccf1a5d12a405978efe277850f7266976c5
carlosrodriguez96 <esemismo96@gmail.com>
2017-03-15 09:05:14 -0500

    Create llamado-php.php
    este archivo comunica al programa con la base de datos

---
+++ www/llamado-php.php
@@ -0,0 +1,16 @@
+<?php
+
+    /**
+     * Este php me permite usar el poder del php con un ejemplo de la tecnología AngularJS...
+     * incluso desde el administrador de este sitio.
+     */
+    include'BD.php';
+    $nuevo_obj=new BD();    // llama la clase BD
+           
+     if( isset( $_GET[ 'cadena' ] ) )
+    {     
+        $valores=$_GET['cadena'];
+        echo  $nuevo_obj->consultar($valores);
+        //echo $sql;
+    }
+  ?>
