2484e156537f15073e1ed6371ad10ee3fb5ddb6e
carlosrodriguez96 <esemismo96@gmail.com>
2017-03-15 09:49:55 -0500

    Update config.php

--- www/config.php
+++ www/config.php
@@ -1,8 +1,16 @@
 <?php 
-	$servidor="localhost";
-	$usuario="root";
-	$clave="";
-	$bd="bd_proyecto";
-	$sn_pruebas="s";
-	$sn_pruebas_log="s";
+/**
+* Desarrollado por:
+* Carlos Arturo rodriguez
+* Jhon Jairo Salazar 
+*/
+/*
+* este archivo contiene todos los parametros de la base de datos.
+*/
+	$servidor="localhost";//nombre del servidor
+	$usuario="root";//nombre del usuario
+	$clave="";//clave del servidor
+	$bd="bd_proyecto";//nombre base de datos
+	$sn_pruebas="s";//ver sql
+	$sn_pruebas_log="s";//ver sql
  ?>
