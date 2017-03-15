da4092ea0404a19e239e86cd1401763d8dfce00d
carlosrodriguez96 <esemismo96@gmail.com>
2017-03-15 08:54:37 -0500

    Update mi_js.js
    esta es la función que envia los datos al llamado-php.php

--- www/js/mi_js.js
+++ www/js/mi_js.js
@@ -1,1 +1,66 @@
-aaa
+
+	
+		var acumuladorApp = angular.module( 'acumuladorApp', [] );        
+        acumuladorApp.controller( "acumuladorAppCtrl",           
+            [ "$scope", "$http",
+            function( $scope, $http )
+            {
+            	$scope.cargar_datos_php=function()
+					                      
+                    {          
+		                    
+		            		
+		            		
+		            		//console.log($scope.lista.length);
+		            		
+		            		var lista=document.getElementById('sintomas');
+		            		//console.log("esta es la seleccion  "+ lista);  		            		
+		            		//&console.log("esta es la cantidad de sintomas seleccones  " +lista.length); 
+		            		var salida="";
+		            		var cadena="";
+
+		            		for (var i = 0; i < lista.length; i++)
+		            		 {	
+			            		   if (lista.item(i).selected) 
+			            		   {
+				            		   	if (salida!="") 
+				            		   	{
+				            		   		salida+=","+lista.item(i).value;
+				            		   		
+
+				            		   	}else{
+				            		   		
+				            		   		salida+=lista.item(i).value;
+				            		   	}
+				            		  
+
+			            		   }                 	
+		            		 } 
+		            		 console.log(salida);
+		            		cadena=salida;
+		            		if(cadena.length>0)
+		            		{
+		            			document.getElementById('carga').innerHTML="<img src='img/carga.gif'>";
+		            			$http.get('llamado-php.php?cadena=' + cadena)
+    							.then(
+    								
+    								function (response) 
+    									{
+    										document.getElementById('carga').innerHTML="";
+    										$scope.campos = response.data.records;
+    									}
+									);
+		            			 
+		                            console.log("valor que deberia llegar al php  "   + cadena);  
+		            		}             
+                    }	
+
+					
+            	}
+
+
+            
+
+            ]
+         );
+	
