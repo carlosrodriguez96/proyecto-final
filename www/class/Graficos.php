62206c885ccfbbcf0647f8d7ebba0eb06c874d7b
carlosrodriguez96 <esemismo96@gmail.com>
2017-03-16 12:06:43 -0500

    Update Graficos.php

--- www/class/Graficos.php
+++ www/class/Graficos.php
@@ -7,43 +7,40 @@
 		*@return	texto		retorna la ubicaccion de la carpeta
 		*/
 		function estilos($carpeta=null)
-			{
-				$salida="";
+		{
+			$salida="";
 
-				$salida=" <link rel='stylesheet'  type='text/css' href='$carpeta/css/bootstrap.css'>
-						 <script src='$carpeta/js/jquerymin.js'></script>
-						 <script src='$carpeta/js/bootstrap.min.js'></script>";
-				return $salida;		 
-			}
-			/**
-			* Esta función se encarga de retornar el encabezado  para que sean impresos desde afuera.
-			* El emcabezado debe estar presente en todos los archivos que muestren resultados al usuario. Es decir, en todas las secciones.
-			*/
+			$salida=" <link rel='stylesheet'  type='text/css' href='$carpeta/css/bootstrap.css'>
+					 <script src='$carpeta/js/jquerymin.js'></script>
+					 <script src='$carpeta/js/bootstrap.min.js'></script>";
+			return $salida;		 
+		}
+		/**
+		* Esta función se encarga de retornar el encabezado  para que sean impresos desde afuera.
+		* El emcabezado debe estar presente en todos los archivos que muestren resultados al usuario. Es decir, en todas las secciones.
+		*/
 		function encabezado($text,$texp)
-			{
-				$salida="";
-				$salida="<div class='page-header'><h1>$text <small>$texp</small></h1></div>";
-				return $salida;
+		{
+			$salida="";
+			$salida="<div class='page-header'><h1>$text <small>$texp</small></h1></div>";
+			return $salida;
 
-			}
-			/**
-			* Esta función se de imprimir el sql.			
-			*/
-		 function imprimir($texto){
+		}
+		/**
+		* Esta función se de imprimir el sql.			
+		*/
+		 function imprimir($texto)
+		 {
 
-			 	include 'config.php';
+			include 'config.php';
 
-			 	 if ($sn_pruebas=="s") {
-			 	 	echo "<p class='bg-success' >".$texto."</p>";
-			 	 	
-			 	 	
-			 	 	
-			 	 }
-			 	 
+			 if ($sn_pruebas=="s") 
+			 {
+				echo "<p class='bg-success' >".$texto."</p>";
+			 }  
 
-			 	
-			 	
-			 }
+			 		 	
+		 }
 		
 		
 	}
