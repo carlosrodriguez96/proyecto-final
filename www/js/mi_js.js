5c415c6f7aed25d925b8b7442817aa98ba57b58b
carlosrodriguez96 <esemismo96@gmail.com>
2017-03-27 09:55:04 -0500

    Update mi_js.js
    en esta ocasión se agrego una nueva función para ejecutar la ayuda

--- www/js/mi_js.js
+++ www/js/mi_js.js
@@ -1,15 +1,11 @@
-/**
-* Desarrollado por:
-* Carlos Arturo rodriguez
-* Jhon Jairo Salazar 
-*/
+
 	
 		var acumuladorApp = angular.module( 'acumuladorApp', [] );        
         acumuladorApp.controller( "acumuladorAppCtrl",           
             [ "$scope", "$http",
-            function( $scope, $http )//inicio de la funcion
+            function( $scope, $http )
             {
-            	$scope.cargar_datos_php=function()//inicio de la funcion cargar_datos_php.
+            	$scope.cargar_datos_php=function()
 					                      
                     {          
 		                    
@@ -17,13 +13,13 @@
 		            		
 		            		//console.log($scope.lista.length);
 		            		
-		            		var lista=document.getElementById('sintomas');//captura el valor de la lista.
+		            		var lista=document.getElementById('sintomas');
 		            		//console.log("esta es la seleccion  "+ lista);  		            		
 		            		//&console.log("esta es la cantidad de sintomas seleccones  " +lista.length); 
 		            		var salida="";
 		            		var cadena="";
 
-		            		for (var i = 0; i < lista.length; i++)//esto se encarga de concatenar la cadena
+		            		for (var i = 0; i < lista.length; i++)
 		            		 {	
 			            		   if (lista.item(i).selected) 
 			            		   {
@@ -43,21 +39,44 @@
 		            		 console.log(salida);
 		            		cadena=salida;
 		            		if(cadena.length>0)
-		            		{
+		            		{	document.getElementById('tabla').style.display = "none";
 		            			document.getElementById('carga').innerHTML="<img src='img/carga.gif'>";
-		            			$http.get('llamado-php.php?cadena=' + cadena)//aqui se envia el calor de la cadena al php.
+		            			
+		            			$http.get('llamado-php.php?cadena=' + cadena  )
     							.then(
     								
     								function (response) 
     									{
     										document.getElementById('carga').innerHTML="";
-    										$scope.campos = response.data.records;//aqui se recive la respuesta del php.
+    										document.getElementById('tabla').style.display = "block";
+    										$scope.campos = response.data.records;
     									}
 									);
 		            			 
 		                            console.log("valor que deberia llegar al php  "   + cadena);  
 		            		}             
-                    }	
+                    }
+                    $scope.cargar_datos_php2=function()
+					                      
+                    {          
+		                    
+		            		
+		            
+		            			var consulta=$scope.consulta;
+		            			$http.get('llamado-php.php?consulta=' + consulta  )
+    							.then(
+    								
+    								function (response) 
+    									{
+    										
+    										$scope.campos = response.data.records;
+    									}
+									);
+		            			 
+		                            console.log("valor que deberia llegar al php  "   + consulta);  
+		            		             
+                    }
+
 
 					
             	}
@@ -67,4 +86,5 @@
 
             ]
          );
+	
 	
