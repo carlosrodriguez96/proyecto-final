4999cef3769658d75aed93ccfb4be608f0d37c7e
carlosrodriguez96 <esemismo96@gmail.com>
2017-04-03 14:20:54 -0500

    Update llamado-php.php
    aquí se modifico para que mediante la variable búsqueda se consulte en la base de datos y traiga la información del termino deseado

--- www/llamado-php.php
+++ www/llamado-php.php
@@ -1,20 +1,23 @@
 <?php
-    /**
-    * Desarrollado por:
-    * Carlos Arturo rodriguez
-    * Jhon Jairo Salazar 
-    */
+
     /**
      * Este php me permite usar el poder del php con un ejemplo de la tecnología AngularJS...
      * incluso desde el administrador de este sitio.
      */
-    include'BD.php';
+    include'class/BD.php';
     $nuevo_obj=new BD();    // llama la clase BD
            
-     if( isset( $_GET[ 'cadena' ] ) )//se pregunta si llegan valores 
+     if( isset( $_GET[ 'cadena' ] ) )
     {     
         $valores=$_GET['cadena'];
-        echo  $nuevo_obj->consultar($valores);//funcion que consulta la base de datos
+        echo  $nuevo_obj->consultar($valores);
         //echo $sql;
+    }
+    if (isset($_GET['busqueda'])) {
+        if ($_GET['busqueda']!="") {
+           
+            echo  $nuevo_obj->consulta();
+        }
+         
     }
   ?>
