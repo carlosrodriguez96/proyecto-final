9cff59deebfbc0b10ca41198a14ad103e2698a84
carlosrodriguez96 <esemismo96@gmail.com>
2017-04-18 20:31:58 -0500

    Add files via upload

---
+++ www/borrando_archivos.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+    /**
+    * 
+    * Este código borra los archivos de la instalación y redirige al sitio. 
+    */
+
+    include( "class/BD.php" );
+    $objeto_verificador = new BD();
+
+    $objeto_verificador->borrar_archivo( "instalador.php" );
+    $objeto_verificador->borrar_archivo( "instalando.php" );
+    header( "location: index.php" );
