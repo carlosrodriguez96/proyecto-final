c21ee4aa195fd207267233e9678554df379ae1e5
carlosrodriguez96 <esemismo96@gmail.com>
2017-04-24 15:11:25 -0500

    Update Verificar.php

--- www/class/Verificar.php
+++ www/class/Verificar.php
@@ -17,10 +17,7 @@ class Verificar
         //echo "El archivo de configuración existe, se procederá a ir al sitio.";
         header( "location: instalador.php" );
     
-    }else{
-            //echo "El archivo no existe, se proceder&aacute; a ir al instalador.";
-            header( "location: index.php" );
-        }
+    }
 	}
 
 	  /**
