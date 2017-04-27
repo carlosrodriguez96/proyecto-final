97efb877a98bd94b95a891099067394d6a97eafa
carlosrodriguez96 <esemismo96@gmail.com>
2017-04-27 15:56:05 -0500

    Update index.php

--- www/index.php
+++ www/index.php
@@ -7,84 +7,83 @@
  -->
 
 <html lang="ES" ng-app="acumuladorApp">
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<head>
-	<title>PetVet</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<head>
+		<title>PetVet</title>
 
 
-	<script type="text/javascript" src="js/angular.min.js"></script>
-	<meta charset="utf-8">
-	<?php 
-		include 'class/BD.php'; //trae las funciones de la pagina BD.php
-		$nuevo_obj=new BD();	// llama la clase BD
-			echo $nuevo_obj->estilos("bootstrap"); // trae la funcion estilos de la clase
-			echo $nuevo_objeto->validar_instalador();
-	?>
-</head>
-<body>
-	<div class="container">
-	<div ng-controller="acumuladorAppCtrl">		
-		<div class="row">
-			<?php  echo $nuevo_obj->encabezado("Diagnóstico","de enfermedades"); ?> 
-		</div>
-		<div class="row" id="con-list" style="display:block;">
-			<div class="col-xs-12 col-md-3 col-lg-3 ">
-				
-              
-				<center>
-                
+		<script type="text/javascript" src="js/angular.min.js"></script>
+		<meta charset="utf-8">
+		<?php 
+			include 'class/BD.php'; //trae las funciones de la pagina BD.php
+			$nuevo_obj=new BD();	// llama la clase BD
+				echo $nuevo_obj->estilos("bootstrap"); // trae la funcion estilos de la clase
+				echo $nuevo_obj->validar_instalador();
+		?>
+	</head>
+	<body>
+		<div class="container">
+		<div ng-controller="acumuladorAppCtrl">		
+			<div class="row">
+				<?php  echo $nuevo_obj->encabezado(); ?> 
+			</div>
+			<div class="row" id="con-list" style="display:block;">
+				<div class="col-xs-12 col-md-3 col-lg-3 ">
+
+
+					<center>
+
+					<?php 	
+						echo $nuevo_obj->traer_informacion("sintoma","tb_sintomas","id_sintomas","sintoma"); // trae la información a mostrar.
+					?>
+			</center>
+
+
+				</div>	
+				<div class="col-xs-12 col-md-8 col-lg-8 ">
+			 <center><div id="carga"> </div></center>
+
 				<?php 	
-					echo $nuevo_obj->traer_informacion("sintoma","tb_sintomas","id_sintomas","sintoma"); // trae la información a mostrar.
-				?>
-                </center>
+						echo $nuevo_obj->imprimir("{{x.abc}}"); // trae la información a mostrar.
 
-				  
-			</div>	
-			<div class="col-xs-12 col-md-8 col-lg-8 ">
-                 <center><div id="carga"> </div></center>
-    			
-    			<?php 	
-    					echo $nuevo_obj->imprimir("{{x.abc}}"); // trae la información a mostrar.
-    
-    				?>
-    		 <div id="tabla">	 
-                    <table class="table table-hover table-bordered" >
-                        <thead>
-                          <tr>
-                            <th>Enfermedad Posible</th>
-                            <th>Sintomas en esta Enfermedad</th>
-                            <th>Sintomas encontrados</th>
-                          </tr>
-                        </thead>
-                         <tbody>
-                          <tr ng-repeat="x in campos">
-                            <td>{{ x.Enfermedad }}</td>
-                            <td>{{ x.conteo_total }}</td>
-                            <td>{{ x.conteo_sintomas }}</td>
-        
-                          </tr>
-                          </tbody>
-                    </table>      
-                     </div>                 
-               
-				 
+					?>
+			 <div id="tabla">	 
+			    <table class="table table-hover table-bordered" >
+				<thead>
+				  <tr>
+				    <th>Enfermedad Posible</th>
+				    <th>Sintomas en esta Enfermedad</th>
+				    <th>Sintomas encontrados</th>
+				  </tr>
+				</thead>
+				 <tbody>
+				  <tr ng-repeat="x in campos">
+				    <td>{{ x.Enfermedad }}</td>
+				    <td>{{ x.conteo_total }}</td>
+				    <td>{{ x.conteo_sintomas }}</td>
+
+				  </tr>
+				  </tbody>
+			    </table>      
+			     </div>                 
+
+
+				</div>
+				<div class="col-xs-12 col-md-1 col-lg-1">
+					<a href="ayuda.php"><button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-question-sign"></span> Ayuda</button></a>
+				</div>
 			</div>
-			<div class="col-xs-12 col-md-1 col-lg-1">
-				<a href="ayuda.php"><button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-question-sign"></span> Ayuda</button></a>
+
+
+
 			</div>
 		</div>
-		
-		
-
-		</div>
-	</div>
-	<script type="text/javascript" src="js/mi_js.js"></script>
-	<script type="text/javascript"></script>
-	
-	
+		<script type="text/javascript" src="js/mi_js.js"></script>
 
 
 
-</body>
+
+
+
+	</body>
 </html>
-
